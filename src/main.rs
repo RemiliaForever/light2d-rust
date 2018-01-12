6ffe9d261d1c965edf3683172900c0867168b355
RemiliaForever <remilia@koumakan.cc>
2018-01-13 01:15:29 +0800

    add rayon

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
 extern crate image;
-extern crate rand;
 extern crate rayon;
-
-use std::io::Error;
+extern crate rand;
 
 use image::ImageBuffer;
 use image::Rgb;
-use rand::Rng;
+use rayon::prelude::*;
 
-const WIDTH: usize = 1920;
-const HEIGHT: usize = 1080;
+const WIDTH: u32 = 1920;
+const HEIGHT: u32 = 1080;
 
+fn basic(x: f32, y: f32, _param: Vec<u8>) -> Rgb<u8> {
+    let red = (1.0 - x) * 255.0;
+    let green = x * 255.0;
+    let blue = y * 255.0;
+    Rgb([red as u8, green as u8, blue as u8])
+}
 
-fn save(path: &str, data: [[[u8; 3]; HEIGHT]; WIDTH]) -> Result<(), Error> {
-    let mut img: image::RgbImage = ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
-    for x in 0..WIDTH {
-        for y in 0..HEIGHT {
-            img.put_pixel(x as u32, y as u32, Rgb(data[x][y]));
-        }
+use std::time::SystemTime;
+use std::error::Error;
+fn gen(fun: fn(f32, f32, Vec<u8>) -> Rgb<u8>, path: &str) -> String {
+    let mut img: image::RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
+    let start = SystemTime::now();
+    let result: Vec<(u32, u32, Rgb<u8>)> = (0..WIDTH * HEIGHT)
+        .into_par_iter()
+        .map(|point: u32| {
+            let x = point / HEIGHT;
+            let y = point % HEIGHT;
+            let xp = x as f32 / WIDTH as f32;
+            let yp = y as f32 / HEIGHT as f32;
+            (x, y, fun(xp, yp, vec![]))
+        })
+        .collect();
+    let stop = SystemTime::now();
+    for p in result {
+        img.put_pixel(p.0, p.1, p.2);
     }
-    img.save(path)
+    match img.save(path) {
+        Ok(()) => match stop.duration_since(start) {
+            Ok(d) => format!("cost: {:.6} s", d.as_secs() as f64 + d.subsec_nanos() as f64 * 1e-9),
+            Err(e) => format!("error: {}", e.description())
+        },
+        Err(e) => format!("error: {}", e.description()),
+    }
 }
 
 
 
 fn main() {
-    let mut data = [[[0; 3]; HEIGHT]; WIDTH];
-    for x in 0..WIDTH {
-        for y in 0..HEIGHT {
-            data[x][y][0] = 255u8;
-            data[x][y][1] = (x as f64 / WIDTH as f64 * 255 as f64) as u8;
-            data[x][y][2] = (y as f64 / HEIGHT as f64 * 255 as f64) as u8;
-        }
-    }
-    let _ = save("./output.png", data);
+    println!("{} => {}", "basic", gen(basic, "./basic.png"));
 }
